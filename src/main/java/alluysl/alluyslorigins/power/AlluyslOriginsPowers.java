7322c5f5861302a9bb8f62155591df1f392f7eaf
Alluysl <alluysl.contact@gmail.com>
2021-06-18 17:44:39 +0200

    Add 'interpolated' alias for 'interpolate'

--- src/main/java/alluysl/alluyslorigins/power/AlluyslOriginsPowers.java
+++ src/main/java/alluysl/alluyslorigins/power/AlluyslOriginsPowers.java
@@ -34,6 +34,7 @@ public class AlluyslOriginsPowers {
                         .add("flip_profile_time", SerializableDataType.BOOLEAN, false)
                         .add("flip_profile_value", SerializableDataType.BOOLEAN, false)
                         .add("interpolate", SerializableDataType.BOOLEAN, true)
+                        .add("interpolated", SerializableDataType.BOOLEAN, true)
                         .add("lerp", SerializableDataType.BOOLEAN, true)
                         .add("up_ticks", SerializableDataType.INT, 0)
                         .addFunctionedDefault("down_ticks", SerializableDataType.INT, data -> data.getInt("up_ticks"))
@@ -74,7 +75,7 @@ public class AlluyslOriginsPowers {
                             data.getString("profile"),
                             data.getBoolean("flip_profile_time"),
                             data.getBoolean("flip_profile_value"),
-                            processAliases(new Boolean[]{data.getBoolean("interpolate"), data.getBoolean("lerp")}, true),
+                            processAliases(new Boolean[]{data.getBoolean("interpolate"), data.getBoolean("interpolated"), data.getBoolean("lerp")}, true),
                             data.getInt("up_ticks"),
                             data.getInt("down_ticks"),
                             data.getBoolean("cyclic"),
